04fec435a1e460261517edc604c63636d9ed7ced
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-09-28 17:19:00 +0700

    Update router by Hiu

--- src/router/index.js
+++ src/router/index.js
@@ -1,45 +1,60 @@
 // src/router/index.js
-import { createRouter, createWebHistory } from 'vue-router';
-import LoginComponent from '@/components/LoginComponent.vue';
-import HomeComponent from '@/components/HomeComponent.vue';
-import DetailComponent from '@/components/DetailComponent.vue';
-
+import { createRouter, createWebHistory } from "vue-router";
+import LoginComponent from "@/components/LoginComponent.vue";
+import HomeComponent from "@/components/HomeComponent.vue";
+import DetailComponent from "@/components/DetailComponent.vue";
+import ReportComponent from "@/components/ReportComponent.vue";
 const routes = [
-  {
-    path: '/auth',
-    name: 'Auth',
-    component: LoginComponent
-  },
-  {
-    path: '/index',
-    name: 'Index',
-    component: HomeComponent
-  },
-  {
-    path: '/home',
-    name: 'Home',
-    component: HomeComponent
-  },
-  {
-    path: '/google-youtube-live-callback',
-    name: 'Home',
-    component: HomeComponent
-  },
-  {
-    path: '/detail',
-    name: 'Detail',
-    component: DetailComponent
-  },
-  {
-    path: '/category',
-    name: 'Category',
-    component: DetailComponent
-  }
+    {
+        path: "/auth",
+        name: "Auth",
+        component: LoginComponent,
+    },
+    {
+        path: "/index",
+        name: "Index",
+        component: HomeComponent,
+    },
+    {
+        path: "/home",
+        name: "Home",
+        component: HomeComponent,
+    },
+    {
+        path: "/google-youtube-live-callback",
+        name: "Home",
+        component: HomeComponent,
+    },
+    {
+        path: "/detail",
+        name: "Detail",
+        component: DetailComponent,
+    },
+    {
+        path: "/change-password",
+        name: "ChangePassword",
+        component: DetailComponent,
+    },
+    {
+        path: "/register",
+        name: "Register",
+        component: MenuComponent,
+    },
+    {
+        path: "/category",
+        name: "Category",
+        component: DetailComponent,
+    },
+    {
+        path: "/report",
+        name: "Report",
+        component: ReportComponent,
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+    history: createWebHistory(),
+    routes,
 });
 
 export default router;
