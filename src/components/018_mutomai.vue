bec518afd9cc4c055c7c5631d0f3e8b8cf80a2c1
hbdiep <hbdiep2004@gmail.com>
2025-09-28 17:32:51 +0700

    Commit lần 2

--- src/components/018_mutomai.vue
+++ src/components/018_mutomai.vue
@@ -2,4 +2,5 @@
     <h1>Tôi là là component tao boi DiepBich</h1>
     <h3>Du lieu nhan duoc tu component cha: {{ dataSet }}</h3>
     <input type="text" v-model="dataChange">
+    <h1>Push lần 2</h1>
 </template>
