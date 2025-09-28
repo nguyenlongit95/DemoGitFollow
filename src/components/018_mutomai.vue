fb61cb6b5d3a648f5af97312e18f4524d590a783
hbdiep <hbdiep2004@gmail.com>
2025-09-28 17:30:07 +0700

    Commit mới của Diệp

---
+++ src/components/018_mutomai.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>Tôi là là component tao boi DiepBich</h1>
+    <h3>Du lieu nhan duoc tu component cha: {{ dataSet }}</h3>
+    <input type="text" v-model="dataChange">
+</template>
